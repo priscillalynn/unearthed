b0663d03c0db42e38d6cfe3e2ad61636d502bb42
priscillalynn <plynn9036@gmail.com>
2023-09-16 17:59:33 -0400

    first commit

---
+++ client/public/scripts/header.js
@@ -0,0 +1,18 @@
+const header = document.querySelector('header');
+const headerContainer = document.createElement('div');
+headerContainer.className = 'header-container';
+
+const headerLeft = document.createElement("div");
+headerLeft.className = "header-left";
+
+const headerLogo = document.createElement("img");
+headerLogo.src = "/logo.png";
+
+const headerTitle = document.createElement("h1");
+headerTitle.textContent = "UnEarthed";
+
+headerLeft.appendChild(headerLogo);
+headerLeft.appendChild(headerTitle);
+
+const headerRight = document.createElement("div");
+headerRight.className = "header-right";
